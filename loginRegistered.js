385f4f94efc4aeaf39bbe505ed8eb908808ba028
tiwariabhi1111 <54528211+tiwariabhi1111@users.noreply.github.com>
2019-09-05 13:37:57 +0530

    updated

--- loginRegistered.js
+++ loginRegistered.js
@@ -2,99 +2,149 @@ let express = require('express');
 //var BodyParser = require('body-parser');
 //var validator = require('express-joi-validator');
 var Joi = require('joi');
+var MongoClient = require('mongodb').MongoClient;
 const jwt = require('jsonwebtoken');
 var app = express();
 app.use(express.json());
-var userRegisterData = [];
+//var userRegisterData = [];
 //user registered validation
-const userRegister = Joi.object().keys({
+const userSchema = Joi.object().keys({
+    name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    firstName: Joi.string().required(),
-    lastName: Joi.string(),
-    phoneNumber: Joi.number().min(100000000).max(9999999999).required()
+    phone: Joi.number().min(100000000).max(9999999999).required()
 })
 // //router for userRegister
 app.post('/register', (req, res) => {
 
-
-    //validation of user
-    Joi.validate(req.body, userRegister, (err, data) => {
+    Joi.validate(req.body, userSchema, (err, data) => {
         if (err) res.send('you entered a invalid data');
         else {
-            let check = 0;
-            userRegisterData.forEach(element => {
-                if (req.body.email == element.email) {
-                    check = 1
-                    res.send('user already exist!!please enter different email id ');
-
-                }
-            })
-            if (check == 0) {
-
-                let password = req.body.password
-
-                try {
-                    var token = jwt.sign({ password }, "privateKey", { algorithm: 'HS256' });
-                    data.password = token;
-                    userRegisterData.push(data);
-                }
-                catch (e) {
-                    res.send("error in jwt sign");
-                }
-                res.send(userRegisterData);
-
+            // let user:{ email, password } = req.body;
+            user = {
+                email: req.body.email,
+                password: req.body.password
             }
-
-        }
-    });
-    //console.log(userRegisterData);
-    res.send('data entered');
-});
-
-//user login
-let loginData = Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required()
-}
-)
-app.get('/login', (req, res) => {
-    //email = req.query.email,
-    //password = req.query.password,
-    Joi.validate(req.query, loginData, (err, data) => {
-        if (err) res.send('invalid format......');
-        else {
-
-            // password = req.query.password;
-            // jwt.sign({ user: password }, 'secretkey', (err, token) => {
-            //     tokenPassword = token;
-            //     //res.json({ password })
-            // })
-            let tokenPassword = req.query.password
-
             try {
-                var token = jwt.sign({ tokenPassword }, "privateKey", { algorithm: 'HS256' });
-                tokenPassword = token;
+
+                MongoClient.connect("mongodb://localhost:27017", async (err, db) => {
+                    var flag = 0;
+                    if (err) throw err;
+                    else {
+                        console.log('database connected......');
+                        let flag = 0;
+                        await db.db('userDatabase').collection('userDetails').find().forEach((element) => {
+                            //console.log((element));
+                            if (user.email == element.email) {
+                                flag = 1
+                            }
+                        });
+                        if (flag === 1) {
+                            res.send('Email alreay registered');
+
+                        }
+                        else {
+
+                            try {
+                                // var token = jwt.sign({ password }, "privateKey", { algorithm: 'HS256' });
+                                //data.password = token;
+                                MongoClient.connect("mongodb://localhost:27017", async (err, db) => {
+
+                                    if (err) throw err;
+                                    else {
+
+
+                                        let data1 = await db.db('userDatabase').collection('userDetails').insert(data);
+                                        res.send('successfully registered');
+                                    }
+                                });
+
+                            }
+                            catch (e) {
+                                res.send("error in jwt sign");
+                            }
+                        }
+                    }
+                });
 
             }
             catch (e) {
                 res.send("error in jwt sign");
             }
-            var checkdata = 0;
-            userRegisterData.forEach(element => {
-                if (element.email == email && element.password == tokenPassword) {
-                    checkdata = 1;
-                    //res.send('you have successfully login...');
-                }
-            })
 
-            if (checkdata == 0) res.send('invalid your email and password')
-            else res.end('you have successfully login....')
+
+
+
+        }
+    });
+})
+// //user login
+let loginData = Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+}
+)
+app.post('/login', (req, res) => {
+
+    // //Joi.validate(req.query, loginData, (err, data) => {
+    //     if (err) res.send('invalid format......');
+    //     else {
+
+    let loginData = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    try {
+        MongoClient.connect("mongodb://localhost:27017", async (err, db) => {
+            var flag = 0;
+            if (err) throw err;
+            else {
+                let flag = 0
+                loginEmail = req.body.email;
+                loginPassword = req.body.password
+                data = await db.db('userDatabase').collection('userDetails').find().forEach(element => {
+                    if (element.email == loginEmail && element.password == loginPassword)
+                        flag = 1;
+                });
+                if (flag == 1) {
+                    jwt.sign(loginData, 'secretkey', (err, token) => {
+
+                        if (!err) res.json({ token });
+                    }
+                    )
+                }
+                else res.send('invalid your email or password');
+            }
+        });
+    }
+    catch (e) {
+        res.send("error in jwt sign");
+    }
+});
+app.post('/validate', (req, res, next) => {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined") {
+        const bearer = bearerHeader.split(" ");
+        const tokenBearer = bearer[1];
+        req.token = tokenBearer;
+        next();
+    }
+    else {
+        res.sendStatus(403);
+    }
+}, (req, res) => {
+    jwt.verify(req.token, "secretkey", (err, data) => {
+        if (err) res.sendStatus(403)
+        else {
+            res.json({
+                msg: "you are successfully login",
+                data: data
+            })
         }
     })
 
-
 })
+
 app.listen(8000, () => {
     console.log('server connected .....');
 });
